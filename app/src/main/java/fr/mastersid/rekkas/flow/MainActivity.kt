a505050674652ea6031c838f4a6f1700fdf15ab8
Imene Rekkas <gi_rekkas@esi.dz>
2021-10-25 12:29:39 +0200

    first commit

---
+++ app/src/main/java/fr/mastersid/rekkas/flow/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.mastersid.rekkas.flow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
